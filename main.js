3751cb690b88979d222b2a9a8a62a2b7491fc314
vaishutajne <106605091+vaishutajne@users.noreply.github.com>
2022-08-28 16:50:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -301,29 +301,6 @@ function getTodos() {
     todoList.appendChild(todoDiv);
   });
 }
-// function filterTodo(e) {
-//   const todos = todoList.childNodes;
-//   todos.forEach(function(todo) {
-//     switch (e.target.value) {
-//       case "all":
-//         todo.style.display = "flex";
-//         break;
-//       case "completed":
-//         if (todo.classList.contains("completed")) {
-//           todo.style.display = "flex";
-//         } else {
-//           todo.style.display = "none";
-//         }
-//         break;
-//       case "incomplete":
-//         if (!todo.classList.contains("completed")) {
-//           todo.style.display = "flex";
-//         } else {
-//           todo.style.display = "none";
-//         }
-//     }
-//   });
-// }
 
 function saveLocalTodos(todo) {
   let todos;
